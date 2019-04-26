844fd079f2202b78f592431b8a10fafeea64b8cb
Pyragon <eldo.imo.rs@hotmail.com>
2019-04-25 22:50:24 -0700

    Fix missing constructor parameter

--- src/main/java/com/cryo/controllers/effects/impl/PoliceEffect.java
+++ src/main/java/com/cryo/controllers/effects/impl/PoliceEffect.java
@@ -3,12 +3,14 @@ package com.cryo.controllers.effects.impl;
 import com.cryo.entities.Effect;
 import com.github.mbelling.ws281x.Ws281xLedStrip;
 
+import java.util.Properties;
+
 public class PoliceEffect extends Effect {
 
     private int stage;
 
-    public PoliceEffect(Ws281xLedStrip strip, int[][] leds) {
-        super(strip, leds);
+    public PoliceEffect(Ws281xLedStrip strip, int[][] leds, Properties settings) {
+        super(strip, leds, settings);
     }
 
     @Override
