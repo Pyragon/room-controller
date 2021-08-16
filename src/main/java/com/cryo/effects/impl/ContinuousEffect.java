8f3db08d488555c01420b459039bc2025d7a31d9
Cody Thompson <eldo.imo.rs@hotmail.com>
2021-08-15 19:49:08 -0700

    add debug message to continuous

--- src/main/java/com/cryo/effects/impl/ContinuousEffect.java
+++ src/main/java/com/cryo/effects/impl/ContinuousEffect.java
@@ -3,9 +3,11 @@ package com.cryo.effects.impl;
 import com.cryo.effects.Effect;
 import com.github.mbelling.ws281x.Color;
 import com.github.mbelling.ws281x.Ws281xLedStrip;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Properties;
 
+@Slf4j
 public class ContinuousEffect extends Effect {
 
 	public ContinuousEffect(int[][] leds, Properties settings) {
@@ -20,6 +22,7 @@ public class ContinuousEffect extends Effect {
 				strip.setPixel(leds[i][k], Color.RED);
 			}
 		}
+		log.info("Rendering continuous effect.");
 		strip.render();
 		return 60_000;
 	}
