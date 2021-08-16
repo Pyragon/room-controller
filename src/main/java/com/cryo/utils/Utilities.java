0e51519d308c94b40a3e977f73bb78ed7aa8f442
Cody Thompson <eldo.imo.rs@hotmail.com>
2021-08-15 19:39:13 -0700

    Fix path to modules

--- src/main/java/com/cryo/utils/Utilities.java
+++ src/main/java/com/cryo/utils/Utilities.java
@@ -253,7 +253,7 @@ public class Utilities {
 				model.put("user", account);
 			if(model.containsKey("404"))
 				prop.put("404", true);
-			module = "./source/modules/"+module+".pug";
+			module = "./web/source/modules/"+module+".pug";
 			String html = Pug4J.render(module, model);
 			prop.put("html", html);
 			return RoomController.getGson().toJson(prop);
