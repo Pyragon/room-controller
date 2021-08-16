88453222a60b1c48f68b2f7f12bf523b8f84b933
Cody Thompson <eldo.imo.rs@hotmail.com>
2021-08-15 18:38:58 -0700

    Add continuous effect

---
+++ src/main/java/com/cryo/effects/impl/ContinuousEffect.java
@@ -0,0 +1,25 @@
+package com.cryo.effects.impl;
+
+import com.cryo.effects.Effect;
+import com.github.mbelling.ws281x.Color;
+import com.github.mbelling.ws281x.Ws281xLedStrip;
+
+import java.util.Properties;
+
+public class ContinuousEffect extends Effect {
+
+	public ContinuousEffect(int[][] leds, Properties settings) {
+		super(leds, settings);
+	}
+
+	@Override
+	public int loop(Ws281xLedStrip strip) {
+		for(int i = 0; i < leds.length; i++) {
+			for(int k = 0; k < leds[i].length; k++) {
+				strip.setPixel(leds[i][k], Color.RED);
+			}
+		}
+		strip.render();
+		return 10_000;
+	}
+}
