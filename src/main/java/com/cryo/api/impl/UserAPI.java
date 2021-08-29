8f7d81c7ecfc15b9d30535c0067d3acdd710de48
Cody Thompson <eldo.imo.rs@hotmail.com>
2021-08-29 16:02:29 -0700

    Add circular image mask to avatars

--- src/main/java/com/cryo/api/impl/UserAPI.java
+++ src/main/java/com/cryo/api/impl/UserAPI.java
@@ -5,10 +5,17 @@ import com.cryo.api.AccountUtils;
 import com.cryo.entities.Account;
 import com.cryo.entities.annotations.Endpoint;
 import com.cryo.entities.annotations.EndpointSubscriber;
+import com.cryo.utils.ImageUtils;
+import com.google.common.io.ByteStreams;
+import com.google.common.net.MediaType;
+import lombok.Cleanup;
 import lombok.extern.slf4j.Slf4j;
 import spark.Request;
 import spark.Response;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.util.Properties;
 import java.util.Random;
 
@@ -32,5 +39,41 @@ public class UserAPI {
 		prop.put("message", MESSAGES[RANDOM.nextInt(MESSAGES.length)]);
 		prop.put("success", true);
 		return RoomController.getGson().toJson(prop);
+	}
+
+	@Endpoint(method="GET", endpoint="/user/avatar")
+	public static String getAvatar(Request request, Response response) {
+		Account account = AccountUtils.getAccount(request);
+		if(account == null || account.getImageName().equals(""))
+			return error404(response, "no account avatar");
+		File file = new File("./data/images/"+account.getImageName());
+		if(!file.exists())
+			return error404(response, "file does not exist");
+		try {
+			BufferedImage image = ImageIO.read(file);
+			if(image == null)
+				return error404(response, "image read null");
+			image = ImageUtils.getCircularImage(image);
+			ByteArrayOutputStream os = new ByteArrayOutputStream();
+			ImageIO.write(image, "png", os);
+			File test = new File("./data/images/test_"+account.getImageName());
+			@Cleanup InputStream in = new ByteArrayInputStream(os.toByteArray());
+			@Cleanup OutputStream testOut = new FileOutputStream(test);
+			ImageIO.write(image, "png", testOut);
+			@Cleanup OutputStream out = new BufferedOutputStream(response.raw().getOutputStream());
+			response.raw().setContentType(MediaType.PNG.toString());
+			response.status(200);
+			ByteStreams.copy(in, out);
+			out.flush();
+			return "";
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return error404(response, "end");
+	}
+
+	public static String error404(Response response, String error) {
+		response.status(404);
+		return error;
 	}
 }
